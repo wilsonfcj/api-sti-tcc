80cd6df68c0d6a4359524f01f64e6a13409655f9
wilsonfcj <wilson.fcj@aluno.ifsc.edu.br>
2020-07-07 00:39:08 -0300

    Atualização conforme o diagrama

--- src/main/java/ifsc/sti/tcc/modelos/desepenho/Desempenho.java
+++ src/main/java/ifsc/sti/tcc/modelos/desepenho/Desempenho.java
@@ -8,4 +8,46 @@ public abstract class Desempenho {
 	private Integer totalRespondidas;
 	private Integer totalQuestaoes;
 	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Integer getTotalErros() {
+		return totalErros;
+	}
+	
+	public void setTotalErros(Integer totalErros) {
+		this.totalErros = totalErros;
+	}
+	
+	public Integer getTotalAcertos() {
+		return totalAcertos;
+	}
+	
+	public void setTotalAcertos(Integer totalAcertos) {
+		this.totalAcertos = totalAcertos;
+	}
+	
+	public Integer getTotalRespondidas() {
+		return totalRespondidas;
+	}
+	
+	public void setTotalRespondidas(Integer totalRespondidas) {
+		this.totalRespondidas = totalRespondidas;
+	}
+	
+	public Integer getTotalQuestaoes() {
+		return totalQuestaoes;
+	}
+	
+	public void setTotalQuestaoes(Integer totalQuestaoes) {
+		this.totalQuestaoes = totalQuestaoes;
+	}
+	
+	
+	
 }
