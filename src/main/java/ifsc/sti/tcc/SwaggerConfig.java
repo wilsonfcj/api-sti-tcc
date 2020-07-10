c0ba3d73c8cced0e3f167119bdaad9a82596319c
wilsonfcj <wilson.fcj@aluno.ifsc.edu.br>
2020-07-10 16:09:27 -0300

    Cors police

--- src/main/java/ifsc/sti/tcc/SwaggerConfig.java
+++ src/main/java/ifsc/sti/tcc/SwaggerConfig.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,31 +15,28 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-	
+public class SwaggerConfig implements WebMvcConfigurer {
+
 	@Bean
 	public Docket productAPI() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("ifsc.sti.tcc"))
-				.paths(PathSelectors.regex("/api.*"))
-				.build()
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("ifsc.sti.tcc")).paths(PathSelectors.regex("/api.*")).build()
 				.apiInfo(createMetaInfo());
-				
+
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("*");
 	}
 
 	private ApiInfo createMetaInfo() {
-		ApiInfo lApiInfo = new ApiInfo("API REST STI",
-				"API REST Para Trabalho de Conclusão de Curso",
-				"1.0",
-				"Uso restrito para estudantes",
-				new Contact("Trabalho de Conclusão de Curso", "", ""),
-				"Chefe Apache License Version 2.0",
-				"http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+		ApiInfo lApiInfo = new ApiInfo("API REST STI", "API REST Para Trabalho de Conclusão de Curso", "1.0",
+				"Uso restrito para estudantes", new Contact("Trabalho de Conclusão de Curso", "", ""),
+				"Chefe Apache License Version 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
 		return lApiInfo;
 	}
-	
+
 }
