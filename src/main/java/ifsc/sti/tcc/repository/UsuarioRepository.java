8b3ac9926ecb4b1aff162fab0c8a40878c8efc03
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2020-11-13 11:14:15 -0300

    Correção e atualização das comfigurações do .POM

--- src/main/java/ifsc/sti/tcc/repository/UsuarioRepository.java
+++ src/main/java/ifsc/sti/tcc/repository/UsuarioRepository.java
@@ -11,11 +11,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	Usuario findById(long id);
 	Usuario findByCpf(String cpf);
-
-  @Query(value = "UPDATE usuario set nome = ?1, email = ?2, fone = ?3, instituicao = ?4, nascimento = ?5, senha = ?6, ano_ingresso = ?7, matricula = ?8  WHERE cpf = '00486870065'", nativeQuery = true)
-  ResultSet update(String nome, String email, String fone,
-		  String Instituicao, Date nascimento,
-		  String senha, int anoIngresso,
-		  long matricula,
-		  String cpf);
+	
+	@Query(value = "UPDATE usuario set nome = ?1, email = ?2, fone = ?3, instituicao = ?4, nascimento = ?5, senha = ?6, ano_ingresso = ?7, matricula = ?8  WHERE cpf = '00486870065'", nativeQuery = true)
+	ResultSet update(String nome, String email, String fone, String Instituicao, Date nascimento, String senha, int anoIngresso, long matricula, String cpf);
 }
