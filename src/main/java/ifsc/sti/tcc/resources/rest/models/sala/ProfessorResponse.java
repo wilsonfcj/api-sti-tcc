d9c2b2b3aaa78cb64d18ea774e17535ec320a469
wilson.junior <wilson.fernandes@sistemainfo.com.br>
2020-12-29 23:07:09 -0300

    Criando salas de simulado

---
+++ src/main/java/ifsc/sti/tcc/resources/rest/models/sala/ProfessorResponse.java
@@ -0,0 +1,40 @@
+package ifsc.sti.tcc.resources.rest.models.sala;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ProfessorResponse {
+	
+	@JsonProperty(value="Id")
+	private Long id;
+	
+	@JsonProperty(value="Nome")
+	private String nome;
+	
+	public ProfessorResponse() {
+		super();
+	}
+
+	public ProfessorResponse(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
